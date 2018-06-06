d02552a96b0d4b02ad69d7d88075e1a9360751a7
zhengwei <zhengwei@007fenqi.com.cn>
2018-06-06 15:41:46 +0800

    删除不需要的代码注释

--- service-consumer/src/main/java/com/runssnail/demo/consumer/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/runssnail/demo/consumer/ServiceConsumerApplication.java
@@ -2,13 +2,10 @@ package com.runssnail.demo.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-//@EnableFeignClients
 public class ServiceConsumerApplication {
 
     public static void main(String[] args) {
--- service-provider/src/main/java/com/runssnail/demo/provider/ServiceProviderApplication.java
+++ service-provider/src/main/java/com/runssnail/demo/provider/ServiceProviderApplication.java
@@ -2,15 +2,12 @@ package com.runssnail.demo.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-//@EnableEurekaClient
 @SpringBootApplication
 @RestController
 /**
@@ -18,38 +15,20 @@ import java.util.logging.Logger;
  */
 public class ServiceProviderApplication {
 
+    private static final Logger LOG = Logger.getLogger(ServiceProviderApplication.class.getName());
+
     public static void main(String[] args) {
         SpringApplication.run(ServiceProviderApplication.class, args);
     }
 
-    private static final Logger LOG = Logger.getLogger(ServiceProviderApplication.class.getName());
-
-
-//    @Autowired
-//    private RestTemplate restTemplate;
-
-//    @Bean
-//    public RestTemplate getRestTemplate() {
-//        return new RestTemplate();
-//    }
-
     @RequestMapping("/hi")
-    public String callHome() {
+    public String callHi() {
         LOG.log(Level.INFO, "calling trace service-provider");
         return "hi";
-        //return restTemplate.getForObject("http://localhost:8989/miya", String.class);
     }
 
-    @RequestMapping("/info")
-    public String info() {
-        LOG.log(Level.INFO, "calling trace service-provider ");
-
-        return "i'm service-provider";
-
-    }
-
-    @Bean
-    public AlwaysSampler defaultSampler() {
-        return new AlwaysSampler();
-    }
+//    @Bean
+//    public AlwaysSampler defaultSampler() {
+//        return new AlwaysSampler();
+//    }
 }
